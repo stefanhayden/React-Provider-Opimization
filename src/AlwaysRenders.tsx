599873681f6bec734a869229727d84a1e737687d
Stefan Hayden <alt255@gmail.com>
2024-04-07 00:01:18 +0000

    cleanup

--- src/AlwaysRenders.tsx
+++ src/AlwaysRenders.tsx
@@ -1,28 +1,39 @@
-import React, { createContext, useCallback, useState, useMemo, useContext, useRef, memo } from 'react';
-import { RenderCounter } from './helpers/RenderCounter';
-import { redStyle, activeModalStyle } from './helpers/styles'
+import React, {
+  createContext,
+  useCallback,
+  useState,
+  useMemo,
+  useContext,
+  useRef,
+  memo,
+} from "react";
+import { RenderCounter } from "./helpers/RenderCounter";
+import { redStyle, activeModalStyle } from "./helpers/styles";
 
 const GlobalModalContextDefault = {
-  activeModal: 'none',
+  activeModal: "none",
   show: (activeModal: string) => {},
-  hide: (activeModal: string) => {},
+  hide: () => {},
 };
 const ModalContext = createContext(GlobalModalContextDefault);
 
 export default function App() {
-  const [activeModal, setActiveModal] = useState<string>('none');
-  const show = useCallback((activeModal: string) => setActiveModal(activeModal), []);
-  const hide = useCallback((activeModal: string) => setActiveModal('none'), []);
+  const [activeModal, setActiveModal] = useState<string>("none");
+  const show = useCallback(
+    (activeModal: string) => setActiveModal(activeModal),
+    [],
+  );
+  const hide = useCallback(() => setActiveModal("none"), []);
 
   const modalProviderValue = useMemo(
     () => ({ activeModal, show, hide }),
-    [activeModal, show, hide]
+    [activeModal, show, hide],
   );
 
   return (
     <ModalContext.Provider value={modalProviderValue}>
-      <div className='App' style={{ flexGrow: 1 }}>
-            <h1>Unoptimized Providers</h1>
+      <div className="App" style={{ flexGrow: 1 }}>
+        <h1>Unoptimized Providers</h1>
         <GlobalModal />
         <Cmp1 />
         <Cmp2 />
@@ -36,42 +47,45 @@ const GlobalModal = memo(() => {
   const { activeModal, hide } = useContext(ModalContext);
   return (
     <div style={redStyle}>
-      <h3>Selected Global Modal: <span style={activeModalStyle}>{activeModal}</span></h3>
-      <button onClick={() => hide('Login')}>hide modal</button>
+      <h3>
+        Selected Global Modal:{" "}
+        <span style={activeModalStyle}>{activeModal}</span>
+      </h3>
+      <button onClick={() => hide("Login")}>hide modal</button>
       <RenderCounter />
     </div>
-  )
-})
+  );
+});
 
 const Cmp1 = memo(() => {
   const { show } = useContext(ModalContext);
   return (
     <div style={redStyle}>
       <h3>Cmp1</h3>
-      <button onClick={() => show('Login')}>show: Login</button>
+      <button onClick={() => show("Login")}>show: Login</button>
       <RenderCounter />
     </div>
-  )
-})
+  );
+});
 
 const Cmp2 = memo(() => {
   const { show } = useContext(ModalContext);
   return (
     <div style={redStyle}>
       <h3>Cmp2</h3>
-      <button onClick={() => show('Pricing')}>show: Pricing</button>
+      <button onClick={() => show("Pricing")}>show: Pricing</button>
       <RenderCounter />
     </div>
-  )
-})
+  );
+});
 
 const Cmp3 = memo(() => {
   const { show } = useContext(ModalContext);
   return (
     <div style={redStyle}>
       <h3>Cmp3</h3>
-      <button onClick={() => show('Logout')}>show: Pricing</button>
+      <button onClick={() => show("Logout")}>show: Pricing</button>
       <RenderCounter />
     </div>
-  )
-})
+  );
+});
--- src/LessRenders.tsx
+++ src/LessRenders.tsx
@@ -1,37 +1,51 @@
-import React, { createContext, useCallback, useState, useMemo, useContext, useRef, memo } from 'react';
-import { RenderCounter } from './helpers/RenderCounter';
-import { greenStyle, activeModalStyle } from './helpers/styles'
-
+import React, {
+  createContext,
+  useCallback,
+  useState,
+  useMemo,
+  useContext,
+  useRef,
+  memo,
+} from "react";
+import { RenderCounter } from "./helpers/RenderCounter";
+import { greenStyle, activeModalStyle } from "./helpers/styles";
 
 const GlobalModalContextDefault = {
-  activeModal: 'none'
+  activeModal: "none",
 };
 const GlobalModalActionsContextDefault = {
   show: (activeModal: string) => {},
-  hide: (activeModal: string) => {},
+  hide: () => {},
 };
 const ModalContext = createContext(GlobalModalContextDefault);
 const ModalActionsContext = createContext(GlobalModalActionsContextDefault);
 
 export default function App() {
-  const [activeModal, setActiveModal] = useState('none');
-  const show = useCallback((activeModal: string) => { setActiveModal(activeModal) }, []);
-  const hide = useCallback((activeModal: string) => { setActiveModal('none') }, []);
+  const [activeModal, setActiveModal] = useState("none");
+  const show = useCallback((activeModal: string) => {
+    setActiveModal(activeModal);
+  }, []);
+  const hide = useCallback(() => {
+    setActiveModal("none");
+  }, []);
 
   const modalProviderValue = useMemo(() => ({ activeModal }), [activeModal]);
-  const modalActionsProviderValue = useMemo(() => ({ show, hide }), [show, hide]);
+  const modalActionsProviderValue = useMemo(
+    () => ({ show, hide }),
+    [show, hide],
+  );
 
   return (
     <ModalActionsContext.Provider value={modalActionsProviderValue}>
-        <ModalContext.Provider value={modalProviderValue}>
-        <div className='App' style={{ flexGrow: 1 }}>
-            <h1>Optimized Providers</h1>
-            <GlobalModal />
-            <Cmp1 />
-            <Cmp2 />
-            <Cmp3 />
+      <ModalContext.Provider value={modalProviderValue}>
+        <div className="App" style={{ flexGrow: 1 }}>
+          <h1>Optimized Providers</h1>
+          <GlobalModal />
+          <Cmp1 />
+          <Cmp2 />
+          <Cmp3 />
         </div>
-        </ModalContext.Provider>
+      </ModalContext.Provider>
     </ModalActionsContext.Provider>
   );
 }
@@ -41,42 +55,45 @@ const GlobalModal = memo(() => {
   const { hide } = useContext(ModalActionsContext);
   return (
     <div style={greenStyle}>
-      <h3>Selected Global Modal: <span style={activeModalStyle}>{activeModal}</span></h3>
-      <button onClick={() => hide('Login')}>hide modal</button>
+      <h3>
+        Selected Global Modal:{" "}
+        <span style={activeModalStyle}>{activeModal}</span>
+      </h3>
+      <button onClick={() => hide("Login")}>hide modal</button>
       <RenderCounter />
     </div>
-  )
-})
+  );
+});
 
 const Cmp1 = memo(() => {
   const { show } = useContext(ModalActionsContext);
   return (
     <div style={greenStyle}>
       <h3>Cmp1</h3>
-      <button onClick={() => show('Login')}>show: Login</button>
+      <button onClick={() => show("Login")}>show: Login</button>
       <RenderCounter />
     </div>
-  )
-})
+  );
+});
 
 const Cmp2 = memo(() => {
   const { show } = useContext(ModalActionsContext);
   return (
     <div style={greenStyle}>
       <h3>Cmp2</h3>
-      <button onClick={() => show('Pricing')}>show: Pricing</button>
+      <button onClick={() => show("Pricing")}>show: Pricing</button>
       <RenderCounter />
     </div>
-  )
-})
+  );
+});
 
 const Cmp3 = memo(() => {
   const { show } = useContext(ModalActionsContext);
   return (
     <div style={greenStyle}>
       <h3>Cmp3</h3>
-      <button onClick={() => show('Logout')}>show: Pricing</button>
+      <button onClick={() => show("Logout")}>show: Pricing</button>
       <RenderCounter />
     </div>
-  )
-})
+  );
+});
